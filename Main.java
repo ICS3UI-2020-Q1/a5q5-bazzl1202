831891832b6fe46ed789c25c5c920e5e0907f81a
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-06 14:30:53 +0000

    Asks for number prints out stars

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Asks user for number between 1 and 10 and prints out that number of stars
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to input a number between 1 and 10
+    System.out.println("Please enter a number between 1 and 10");
+    // create a variable for user input
+    int integer = input.nextInt();
+
+    for(int count = 1; count <= integer; count++){
+      System.out.print("*");
+    }
     
   }
 }
